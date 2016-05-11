121b8247af7f6970450be27678041bbe967a9d11
xf <729219697@qq.com>
2016-05-11 22:34:28 +0800

    consumption update

--- qtx/app/Models/type/Consumption.php
+++ qtx/app/Models/type/Consumption.php
@@ -9,7 +9,7 @@ class consumption extends Model
     protected $table = 'qtx_consumption';
     public $timestamps = false;
     protected $primaryKey = 'consumption_id';
-
+    protected  $fillable = ['consumption_id','consumption_name','status','create_time','update_time'];
     /**
      * 根据主键id,获取单条记录
      * @params $id 表中cosumption_id
