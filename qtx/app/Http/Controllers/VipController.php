79b868790b488a05d4483f227278823d6e254f88
xinmoumou <461093845@qq.com>
2016-05-12 08:44:26 +0800

    会员管理C层

--- qtx/app/Http/Controllers/VipController.php
+++ qtx/app/Http/Controllers/VipController.php
@@ -6,11 +6,102 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use App\Models\vip\Vip;
+use Request;
+use DB;
 
-
-
+header("content-type:text/html;charset=utf-8");
     class VipController extends Controller {   
-        public function index() {   
-            return "asd";
-        }   
+
+    	/**
+		 *	会员管理首页
+		 *	分页，返回查询数据
+    	*/
+        public function index() 
+        {   
+        	$model = new Vip();
+        	$arr = $model->sel_user();
+            return view('vip/list',['arr' => $arr]);
+        }
+        /**
+		 *	会员管理
+		 *	删除，批量删除,接收id，删除数据
+    	*/
+        public function vipdel()
+        {	
+        	$id = $_GET['id'];
+        	$model = new Vip();
+        	$str = $model->del_user($id);
+        	if($str){
+        		echo "1";
+        	}else{
+        		echo "0";
+        	}
+        }
+        /**
+		 *	会员管理
+		 *	跳转修改页面
+    	*/ 
+    	public function vipupdate()
+    	{
+    		$id = $_GET['id'];
+    		$model = new Vip();
+    		$arr = $model->seluser($id);
+    		return view('vip/vipupdate',['arr' => $arr]);
+    	}
+        /**
+         *   会员管理
+         *   修改页面表单提交
+         *   获取修改时间，本次登陆ip
+        */
+        public function vipfrom()
+        {   
+            $model = new Vip();
+            $arr = $_POST;
+            $id = $arr['user_id'];
+            $arr['updated_time'] = time();
+            //print_r($arr);die;
+            $arr['user_lastip'] = $_SERVER['SERVER_ADDR'];
+            $str = $model->updatevip($arr,$id);
+            if($str){
+                echo "<script>alert('修改成功');location.href='/vip';</script>";
+            }else{
+                 echo "<script>alert('修改失败');location.href='/vip';</script>";
+            }
+        }
+        /**
+         *   会员管理
+         *   跳转添加页面，表单
+        */
+        public function vipinsert()
+        {
+            return view('vip/vipinsert');
+        }
+        /**
+         *   会员管理
+         *   添加表单，提交的数据
+        */
+        public function vipinsertfrom()
+        {
+            $arr = Request::all();
+            $arr['create_time'] = time();
+            $arr['updated_time'] = time();
+            $arr['user_lastlogin'] = time();
+            $arr['user_lastip'] = $_SERVER['SERVER_ADDR'];
+            $model = new Vip();
+            $str = $model->insertvip($arr);
+            if($str){
+                 echo "<script>alert('添加成功');location.href='/vip';</script>";
+             }else{
+                 echo "<script>alert('添加失败');location.href='/vip';</script>";
+             }
+        }
+        /**
+         *   会员等级管理
+         *   主页显示
+        */
+        public function rank()
+        {
+            return view('vip/rank');
+        }
     }  
